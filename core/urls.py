395a1d0a6b921e178d14e5ff42e40294695f26fe
mahmudovah <mahmudovahadija.0831@gmail.com>
2026-02-24 16:17:17 +0500

    ovqatlarni qo'shish mumkin

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index_view, name='index'),
     path('contact/', views.contact_view, name='contact'),
-    path('meal/<int:pk>/', views.meal_detail, name='meal_detail')
+    path('meal/<int:pk>/', views.meal_detail, name='meal_detail'),
+    path('meal/add', views.meal_add, name='meal_add')
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import Comment, Meal
+from .models import Comment, Meal, Ingredient
 
 # Create your views here.
 
@@ -8,6 +8,7 @@ def index_view(request):
     print(meals)
     context = {'meals':meals}
     return render(request, 'index.html', context)
+
 
 def meal_detail(request, pk):
     meal = Meal.objects.get(id=pk)
@@ -24,5 +25,28 @@ def meal_detail(request, pk):
         return redirect("meal_detail", meal.id)
     return render(request, "meal_detail.html", context={"meal": meal,"comments":comments})
 
+
 def contact_view(request):
     return render(request, "contact.html")
+
+
+def meal_add(request):
+    ingredients=Ingredient.objects.all()
+
+    if request.method == "POST":
+        name = request.POST.get("name")
+        description =request.POST.get("description")
+        image = request.FILES.get("image")
+        ingredient_ids = request.POST.getlist("ingredients")
+        status = request.POST.get("status")
+
+        meal = Meal.objects.create(
+                name=name,
+                description=description,
+                image=image,
+                status=status
+            )        
+        meal.ingredients.set(ingredient_ids)
+
+        return redirect("meal_add")
+    return render(request, "meal_add.html", {"ingredients":ingredients})
